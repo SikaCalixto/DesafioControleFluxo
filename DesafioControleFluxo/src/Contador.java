39bf6484f4a6c91178ff2e962110d633a7f68e84
Simone Dias Calixto <simonefulgencio@hotmail.com>
2024-05-28 11:33:12 -0300

    Desafio _Controle_De_Fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Contador {
+    
+	public static void main(String[] args) {
+       
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+			
+			}catch (ParametrosInvalidosException exception) {
+			
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println ("O primeiro parâmetro deve ser menor que o segundo");
+		}
+		terminal.close();
+	}
+	static void contar(int parametroUm,int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if(parametroDois < parametroUm){
+		 throw new ParametrosInvalidosException();
+		}
+		  
+
+		int contagem = parametroDois - parametroUm;
+		
+		for (int conta =0; conta <= contagem; conta++){
+		System.out.println(conta);
+		}
+	
+		}
+	
+		}
+	
+    
+
---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+public class ParametrosInvalidosException extends Exception{
+
+    public static void main(String[] args) {
+        System.out.println("O primeiro parâmetro deve ser menor que o segundo");
+    }
+
+}
